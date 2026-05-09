401830b63ab0e6bb7c45d52fe237e805bac57ee4
madhan bandi <madhannn25@gmail.com>
2026-05-09 14:04:12 +0530

    add cors

--- src/main/java/com/taskmanager/teamtaskmanager/config/SecurityConfig.java
+++ src/main/java/com/taskmanager/teamtaskmanager/config/SecurityConfig.java
@@ -42,7 +42,8 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "http://localhost:5500",
+                "https://team-task-manager-production-ee74.up.railway.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
